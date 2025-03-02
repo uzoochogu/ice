39c113790fdbeed1ee65f57f71e6b400b42531cc
uzo <uzoochogu@yahoo.com>
2025-03-02 22:23:28 +0100

    Properly use .env.cmake , rename and include in correct position

---
+++ .env.cmake
@@ -0,0 +1,9 @@
+# If using vcpkg, this is automatically set, but you can set this manually
+# or use here to override it.
+# Vulkan location
+# unset(VULKAN_SDK)
+# unset(Vulkan_INCLUDE_DIRS)
+# unset(Vulkan_LIBRARIES)
+# set(VULKAN_SDK "C:/dev/VulkanSDK" CACHE PATH "Vulkan SDK" FORCE)
+# set(Vulkan_INCLUDE_DIRS "C:/dev/VulkanSDK/include" CACHE PATH "Vulkan Include Directory" FORCE)
+# set(Vulkan_LIBRARIES "C:/dev/VulkanSDK/lib/vulkan-1.lib" CACHE PATH "Vulkan Include Directory" FORCE)
